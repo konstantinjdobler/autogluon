eb95f19a3d26b9552919ade371e78a0dbc0cc127
mitalbert <mitalbert@github.com>
2020-06-20 19:47:55 +0200

    made adam the default optimizer

--- autogluon/contrib/enas/enas_utils.py
+++ autogluon/contrib/enas/enas_utils.py
@@ -8,7 +8,7 @@ def default_reward_fn(metric, net):
     reward = metric * ((net.avg_latency / net.latency) ** 0.07)
     return reward
 
-def init_default_train_args(batch_size, net, epochs, iters_per_epoch):
+def init_sgd_train_args(batch_size, net, epochs, iters_per_epoch):
     train_args = {}
     base_lr = 0.1 * batch_size / 256
     lr_scheduler = gcv.utils.LRScheduler('cosine', base_lr=base_lr, target_lr=0.0001,
@@ -16,5 +16,16 @@ def init_default_train_args(batch_size, net, epochs, iters_per_epoch):
     optimizer_params = {'wd': 1e-4, 'momentum': 0.9, 'lr_scheduler': lr_scheduler}
     train_args['trainer'] = gluon.Trainer(net.collect_params(), 'sgd', optimizer_params)
     train_args['batch_size'] = batch_size
+    train_args['criterion'] = gluon.loss.SoftmaxCrossEntropyLoss()
+    return train_args
+
+def init_default_train_args(batch_size, net, epochs, iters_per_epoch):
+    train_args = {}
+    base_lr = 0.1 * batch_size / 256 
+    lr_scheduler = gcv.utils.LRScheduler('cosine', base_lr=base_lr, target_lr=0.0001, 
+     nepochs=epochs, iters_per_epoch=iters_per_epoch) 
+    optimizer_params = {'wd': 1e-4, 'lr_scheduler': lr_scheduler} 
+    train_args['trainer'] = gluon.Trainer(net.collect_params(), 'adam', optimizer_params) 
+    train_args['batch_size'] = batch_size 
     train_args['criterion'] = gluon.loss.SoftmaxCrossEntropyLoss()
     return train_args
