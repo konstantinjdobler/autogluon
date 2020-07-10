f49c2477187252ce31e314beabbe7e5f63b0f14c
LukasBudach <lukas.budach@student.hpi.de>
2020-07-10 11:54:05 +0200

    init commit

--- autogluon/contrib/enas/enas_scheduler.py
+++ autogluon/contrib/enas/enas_scheduler.py
@@ -24,7 +24,7 @@ class ENAS_Scheduler(object):
     """
     def __init__(self, supernet, train_set='imagenet', val_set=None,
                  train_fn=default_train_fn, eval_fn=default_val_fn, post_epoch_fn=None, post_epoch_save=None,
-                 train_args={}, val_args={}, reward_fn=default_reward_fn,
+                 post_epoch_eval=None, train_args={}, val_args={}, reward_fn=default_reward_fn,
                  num_gpus=0, num_cpus=4,
                  batch_size=256, epochs=120, warmup_epochs=5,
                  controller_lr=1e-3, controller_type='lstm',
@@ -46,6 +46,7 @@ class ENAS_Scheduler(object):
         self.reward_fn = reward_fn
         self.post_epoch_fn = post_epoch_fn
         self.post_epoch_save = post_epoch_save
+        self.post_epoch_eval = post_epoch_eval
         self.checkname = checkname
         self.plot_frequency = plot_frequency
         self.epochs = epochs
