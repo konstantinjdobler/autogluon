776587f7d31891f4ea863cf9f7a4e40debaf2fde
MIDL20T1 <midl20t1@fb10dl04.dl.hpi>
2021-01-05 17:48:42 +0000

    add print

--- autogluon/contrib/enas/enas_scheduler.py
+++ autogluon/contrib/enas/enas_scheduler.py
@@ -342,6 +342,7 @@ class ENAS_Scheduler(object):
             # sample controller_batch_size number of configurations
             configs, log_probs, entropies = self._sample_controller()
             average_config = np.zeros(len(self.supernet.kwspaces))
+            print(configs)
             for i, batch in enumerate(self.val_data):
                 if i >= self.controller_batch_size: break
                 average_config += np.array([v for v in configs[i].values()])
